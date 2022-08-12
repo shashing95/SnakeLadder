62003622d7ef91d02806d78daae83e30ba412c11
shashi <shashidharngg@gmail.com>
2022-08-12 22:53:37 +0530

    start position

---
+++ src/com/bridgelabz/snakeladder/Positionzero.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.snakeladder;
+
+public class Positionzero {
+
+	public static void main(String[] args) {
+		
+		int startPosition=0;
+		System.out.println("Player position at start is : "+startPosition);
+	}
+		
+		
+	}
+
