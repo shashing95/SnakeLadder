883b6ac461023f510945fd0c44d5265befedb566
shashi <shashidharngg@gmail.com>
2022-08-14 23:01:53 +0530

    added:dies rolls

---
+++ src/com/bridgelabz/snakeladder/DieRolls.java
@@ -0,0 +1,22 @@
+package com.bridgelabz.snakeladder;
+
+public class DieRolls {
+
+	public static void main(String[] args) {
+		int Position = 0;
+		System.out.println("Welcome into Snake and Ladder program !!");
+		System.out.println("Your Starting Position is : " + Position);
+		Dice();
+	}
+	public static void Dice(){
+		int Dice = (int)Math.floor(Math.random()*10)%6+1;
+		System.out.println("Your Dice Value is : " +Dice);
+	}
+
+
+		
+
+
+	}
+
+
